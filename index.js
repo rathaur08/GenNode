624a79356a423cff7b0b6e67e8fc95b5c7bc8816
Sunny <sunnyrathaur2444@gmail.com>
2025-10-14 23:00:07 +0530

    OpenAI setup and generate key

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-import { data } from './temp.js';
+import OpenAI from "openai";
 
-console.log(data);
+const openai = new OpenAI({
+  apiKey: "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",
+});
 
-const project = "GenAI App";
-console.log(`Welcome to the ${project}!`);
+const response = await openai.responses.create({
+//   model: "gpt-5-nano",
+//   model: "gpt-5",
+  model: "gpt-4o-mini",
+  input: "Apple color is",
+  store: true,
+});
+
+
+console.log(response);
+
+
+
+// response.then((result) => console.log(result.output_text));
