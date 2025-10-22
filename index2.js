3cc79fda9e029bf5620ee6aaece7b67dd8d6e3f2
rathaur08jsg <sunny.rathaur@jetsetgo.in>
2025-10-22 15:23:51 +0530

    Update Code - Generate Image with gpt-image-1 model

--- index2.js
+++ index2.js
@@ -6,12 +6,30 @@ dotenv.config();
 
 const client = new OpenAI({ apiKey: process.env.TEST_KEY });
 
+// const main = async () => {
+//   const responce = await client.responses.create({
+//     model: "dall-e-2",
+//     input: "Generate image for a baby dog in BUS",
+//     size: "512x512",
+//     response_format: "b64_json",
+//     n: 1,
+//     store: true,
+//   })
+//   console.log("responce", responce);
+//   const image_data = responce.data[0].b64_json;
+//   const path = "./generated_image.png";
+//   const buffer = Buffer.from(image_data, 'base64');
+//   writeFileSync(path, buffer);
+//   console.log(`Image saved to ${path}`);
+// }
+// main();
+
+// Code - Generate Image with gpt-image-1 model
+
 const main = async () => {
   const responce = await client.responses.create({
-    model: "dall-e-2",
-    input: "Generate image for a baby dog in BUS",
-    size: "512x512",
-    response_format: "b64_json",
+    model: "gpt-image-1",
+    input: "Generate image for a girl",
     n: 1,
     store: true,
   })
