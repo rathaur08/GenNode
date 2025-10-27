5c5f9fb986973e16995091a67a6e342054ec80db
rathaur08jsg <sunny.rathaur@jetsetgo.in>
2025-10-27 17:37:07 +0530

    Update Code - Gemini Basic Config Params Explained

--- indexGemAi.js
+++ indexGemAi.js
@@ -8,7 +8,15 @@ const geminiClient = new GoogleGenAI({ apiKey: process.env.GEMINIAI_API_KEY });
 async function main() {
   const response = await geminiClient.models.generateContent({
     model: 'gemini-2.5-flash',
-    contents: 'Telln 5 Fruits names',
+    contents: 'Tell me a joke about programmers.',
+    config: {
+      temperature: 0.7,
+      // thinkingConfig: {
+      //   includeThoughts: true,
+      //   thinkingBudget: 100,
+      // },
+      systemInstruction: "tell me answers in 100 words",
+    }
   });
   console.log('Gemini Response:', response.text);
 }
