981e957c082155b66d523981f25a723d49bf1c8c
rathaur08jsg <sunny.rathaur@jetsetgo.in>
2025-10-30 17:24:27 +0530

    Update Code - OpenAI || Add Data in Chroma DB using Node.js | Vector Database

--- chroma.js
+++ chroma.js
@@ -1,6 +1,8 @@
 import { CloudClient } from 'chromadb';
 import { DefaultEmbeddingFunction } from '@chroma-core/default-embed';
 import dotenv from 'dotenv';
+import { GenerateContentResponse } from '@google/genai';
+import { generateEmbedding } from './index11Embedding.js';
 
 dotenv.config();
 
@@ -11,12 +13,32 @@ const client = new CloudClient({
 });
 
 async function main() {
-  const collection = await client.createCollection({
-    name: 'sample-data',
+  const colorData = generateEmbedding("green")
+  const collection = await client.getOrCreateCollection({
+    name: 'colors',
     embeddingFunction: new DefaultEmbeddingFunction(), // ✅ required
+  })
+  collection.add({
+    ids: ['1'],
+    documents: ['green'],
+    embeddings: [colorData[0].embedding],
   });
 
   console.log('Collection created:', collection.name);
 }
+
+// async function main() {
+//   // const collection = await client.createCollection({
+//   const collection = await client.getOrCreateCollection({
+//     name: 'animal',
+//     embeddingFunction: new DefaultEmbeddingFunction(), // ✅ required
+//   })
+//   collection.add({
+//     ids: ['2'],
+//     documents: ['dog'],
+//     embeddings: [[1, -1, -1]],
+//   });
+//   console.log('Collection created:', collection.name);
+// }
 
 main().catch(console.error);
