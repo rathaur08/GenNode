baf27d2a2ec182ea6e51e21d76e973717e2110ae
rathaur08jsg <sunny.rathaur@jetsetgo.in>
2025-12-04 15:52:34 +0530

    Update Code - OpenAI - Batch or Stream Methods in LangChain Explained #langchain #38

--- LangChain-tut/index.js
+++ LangChain-tut/index.js
@@ -9,8 +9,24 @@ const model = new ChatOpenAI({
 })
 
 async function chat() {
-  const response = await model.invoke("Tell 5 frrits name")
-  console.log(response.content);
+  // const response = await model.invoke("Tell 5 frrits name")
+  // console.log(response.content);
+
+  // const response = await model.batch([
+  //   "top 5 country name",
+  //   "top cricketers name in world"
+  // ])
+  // // console.log(response.content);
+  // for (let i = 0; i < response.length; i++) {
+  //   console.log(response[i].content);
+  // }
+
+  const response = await model.stream("top cricketers name in world")
+  // console.log(response.content);
+  for await (const chunk of response) {
+    console.log(chunk.content);
+  }
+
 }
 
 chat();
