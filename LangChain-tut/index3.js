4c18b5aaea88a6f7694587bef4904f4dfd622560
rathaur08jsg <sunny.rathaur@jetsetgo.in>
2025-12-04 17:28:54 +0530

    Update Code - fromTemplate ChatPromptTemplate in LangChain Explained

---
+++ LangChain-tut/index3.js
@@ -0,0 +1,29 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts"
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const model = new ChatOpenAI({
+  model: "gpt-4o-mini",
+  openAIApiKey: process.env.OPENAI_API_KEY,
+})
+
+async function chat() {
+  const prompt = ChatPromptTemplate.fromTemplate("tell me about {topic} on {words} Words");
+
+  //   ["system", "explane topic within 50 words"],
+  //   ["human", `tell me about {topic} with example`]
+  // ])
+
+  const formattedMsg = await prompt.formatMessages({
+    topic: "Node.JS",
+    words: 20
+  });
+
+  const response = await model.invoke(formattedMsg)
+  console.log(response.content);
+
+}
+
+chat();
