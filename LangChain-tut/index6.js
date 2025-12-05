8146e31631da0798181196c36ceb1ec6b736761d
rathaur08jsg <sunny.rathaur@jetsetgo.in>
2025-12-05 16:55:14 +0530

    Code - Search in In-Memory DB using LangChain | Retriever Config #42

--- LangChain-tut/index6.js
+++ LangChain-tut/index6.js
@@ -12,13 +12,29 @@ const mydata = [
   "I an Working in Jetsetgo"
 ];
 
+const question = "what is your name ?";
+
 async function main() {
+  // Data Store in Memory -------------
   const vectorStore = new MemoryVectorStore(new OpenAIEmbeddings())
   await vectorStore.addDocuments(mydata.map(
     item => new Document({ pageContent: item })
   ))
 
-  console.log(vectorStore.memoryVectors);
+  // console.log(vectorStore.memoryVectors);
+  const retrieve = vectorStore.asRetriever({
+    k: 1, // result length
+  })
+
+  // Get Data in Memory Result and Print -------------
+  const results = await retrieve._getRelevantDocuments(question);
+  // console.log(results);
+  const resultContent = results.map(
+    item => item.pageContent
+  )
+  console.log(resultContent);
+
+  // 
 
 }
 
